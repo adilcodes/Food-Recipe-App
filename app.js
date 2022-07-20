986abf5243275f76ef1564cc943203b8333b61f3
Adil <ascodesofficial@gmail.com>
2022-07-20 07:58:27 +0500

    Adding a new feature + fetching titles of 10 foods and displaying them

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+// Targeting DOM Elements
 let ingredientsContainer = document.querySelector(".ingredients");
 
-let Recipe = {
+// Fetching and Storing Data in Objects
+let Foods = {
     apiKey : "dacf0e5944b4444fb449d9333e5c2b4c",
-    fetchRecipe : function(food){
+    fetchFoods : function(food){
         fetch("https://api.spoonacular.com/recipes/complexSearch?query="
         + food
-        + "&number=1&fillIngredients=true&apiKey="
+        + "&number=10&fillIngredients=true&apiKey="
         + this.apiKey
         )
         .then((response) => response.json())
@@ -14,33 +16,38 @@ let Recipe = {
     },
     
     displayData : function(data){
-        const {title} = data["results"][0];
-        // fetching ingredients and their images Start
-        let ingredients = data["results"][0]["missedIngredients"];
-        ingredients.map((item) => {
-            return (
-                ingredientsContainer.innerHTML +=
-                    `<div class="ingredientCard">
-                        <div class="ingredientCard-img flex-box">
-                            <img src="${item['image']}">
-                        </div>
-                        <div class="ingredientCard-text">
-                            <p>${item['original']}</p>
-                        </div>
-                    </div>`
+        // Fetching Food titles and their images and displaying them using map function
+        ingredientsContainer.innerHTML = "";
+        let titles = data["results"];
+        titles.map((foodItem) => {
+            return(
+                ingredientsContainer.innerHTML += 
+                `<div class="food-suggestions">
+                    <table>
+                        <tr class="flex-box">
+                            <td class="flex-box">
+                                <img src="${foodItem['image']}" alt="${foodItem['title']}" width="30">
+                            </td>
+                            <td>
+                                <p>${foodItem['title']}</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>`
             )
         });
-        // fetching ingredients and their images End
+        
     },
 };
 
 
+// Search
 let searchBar = document.querySelector("#searchBar");
 let searchBtn = document.querySelector("#searchBtn");
 
 searchBtn.addEventListener("click", () => {
     if(searchBar.value !== ""){
-        Recipe.fetchRecipe(searchBar.value); 
+        Foods.fetchFoods(searchBar.value); 
     }else{
         alert("Enter The Correct Food Item");
     }
@@ -49,9 +56,29 @@ searchBtn.addEventListener("click", () => {
 searchBar.addEventListener("keyup", (event) => {
     if(event.key == "Enter"){
         if(searchBar.value !== ""){
-            Recipe.fetchRecipe(searchBar.value);
+            Foods.fetchFoods(searchBar.value);
         }else{
             alert("Enter The Correct Food Item");
         } 
     }
 });
+
+
+
+
+// fetching ingredients and their images Start
+// let ingredients = data["results"][0]["missedIngredients"];
+// ingredients.map((item) => {
+//     return (
+//         ingredientsContainer.innerHTML +=
+//             `<div class="ingredientCard">
+//                 <div class="ingredientCard-img flex-box">
+//                     <img src="${item['image']}">
+//                 </div>
+//                 <div class="ingredientCard-text">
+//                     <p>${item['original']}</p>
+//                 </div>
+//             </div>`
+//     )
+// });
+// fetching ingredients and their images End
